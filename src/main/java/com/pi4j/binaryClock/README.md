762dffa5ea264902e871a354f091e52b92e48676
Tom Aarts <taarts1@gmail.com>
2024-05-15 13:18:55 -0500

    binary clock first commit

---
+++ src/main/java/com/pi4j/binaryClock/README.md
@@ -0,0 +1,7 @@
+
+
+
+-b 0x01 -a1 0x20 -a2 0x22 -x 6
+
+-b hex value bus    -a1 hex value first MCP address  " +
+"-a2 hex value second MCP address -t trace   -x resetPin -r doReset  
---
+++ src/main/java/module-info.java
@@ -0,0 +1,29 @@
+module com.pi4j.devices{
+
+    // Pi4J MODULES
+    requires com.pi4j;
+   // requires com.pi4j.plugin.pigpio;
+  //  requires com.pi4j.library.pigpio;
+
+    // SLF4J MODULES   LOG4J
+    requires org.slf4j;
+    requires org.slf4j.simple;
+
+   // requires org.apache.logging.log4j;
+   // requires org.apache.logging.log4j.core;
+
+
+    requires java.logging;
+    requires jdk.unsupported;
+    requires com.pi4j.plugin.linuxfs;
+    requires java.desktop;
+    requires com.pi4j.plugin.gpiod;
+
+    uses com.pi4j.extension.Extension;
+    uses com.pi4j.provider.Provider;
+
+    // allow access to these classes
+
+    exports com.pi4j.binaryClock;
+
+    }
